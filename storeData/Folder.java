443b98daa72a35441a5df832767d0d534eb1fabe
rajkiran <rajkiran.one@gmail.com>
2018-12-08 17:40:10 +0100

    Event name first and folder creation second

--- storeData/Folder.java
+++ storeData/Folder.java
@@ -29,21 +29,8 @@ public class Folder {
 	
 	
 	public void gotoFolder(){
-	//System.out.println("Welcome to Your database");    
+   
 		System.out.print("\n");
-//        System.out.print("1. Create Folder\n");
-//        System.out.print("2. Create Event\n");
-//        System.out.print("3. Add Event to a Folder\n");
-//        System.out.print("4. Delete Folder\n");
-//        System.out.print("5. Delete Event\n");
-//          System.out.print("2. Exit\n");
-
-         
-//      System.out.println("Enter your choice");    
-//      int c_choice=keyboard.nextInt();
-
-//        if (c_choice == 1) {
-
         Scanner name=new Scanner(System.in);    
         System.out.println("Enter you Event Name");    
         String e_name=name.nextLine();
@@ -54,21 +41,7 @@ public class Folder {
         stmt1.setString(1,e_name);        
         stmt1.execute();
         System.out.println("------------------------");    
-        	   
-        System.out.println("Event name :"+ " "+e_name);		   
-        System.out.print("Enter your folder name");
-        Scanner keyboard=new Scanner(System.in);
-        String f_name = keyboard.nextLine();
-        System.out.println("Event name :"+ " "+f_name); 
-        
-
-        PreparedStatement stmt2=conn.prepareStatement("insert into folder_names(f_name) values(?)");      
-        stmt2.setString(1,f_name);
-        stmt2.execute();
-        System.out.println("--------------------");  
-        System.out.println("Folder Successfully Created");  
-        System.out.println("--------------------\n");  
-        System.out.println("Folder Names: \n");
+        System.out.println("Event name :"+ " "+e_name);		
         
         ResultSet rs = stmt.executeQuery("select * from folder_names");
         int i=1;
@@ -76,36 +49,31 @@ public class Folder {
         while (rs.next()) {
         	a = i++;
             String folderName = rs.getString("f_name");
-            System.out.println( + a + ":" + folderName );
-
-        }
-   
-        gotoFolder();       
-        	
+            System.out.println( + a + ": " + folderName );
+        } 
+        
+        System.out.print("Enter your folder name (if no folder exist above, create one)");
+        Scanner keyboard=new Scanner(System.in);
+        String f_name = keyboard.nextLine();
+        System.out.println("Folder name :"+ " "+f_name); 
+        
+        
+        
+        PreparedStatement stmt2=conn.prepareStatement("insert into folder_names(f_name) values(?)");      
+        stmt2.setString(1,f_name);
+        stmt2.execute();
+        System.out.println("--------------------");  
+        System.out.println("Folder Successfully Created");  
+        System.out.println("--------------------\n");  
+//        System.out.println("Folder Names: \n");
+        
+  
+        gotoFolder();            	
         }               
     catch(Exception e)    
     {System.out.println(e);
     gotoFolder();}    
         }
-//        else if (c_choice == 2) {
-//        connect();
-//      	  
-//        }else if(c_choice == 3) {
-//        	
-//        	System.out.println("Bye..");
-//        	
-//        }else if(c_choice ==4) {
-//        	
-//        }
-        
-//        else {
-//        	System.out.println("-------------");
-//        	System.out.println("Oops! Enter your choice between 1 and 2");
-//        	System.out.println("-------------");
-//        	gotoFolder();
-//        }
-
-   
 
 }   
 
